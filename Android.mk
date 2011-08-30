d980e9988015b3d3c45ab6473a558ab53801127b
Kyan He <kyan.ql.he@gmail.com>
2011-08-30 17:26:42 +0800

    Fix Android.mk

--- Android.mk
+++ Android.mk
@@ -3,44 +3,45 @@ LOCAL_PATH:= $(call my-dir)
 # We need to build this for both the device (as a shared library)
 # and the host (as a static library for tools to use).
 
-# build static library
+
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libfb2png
-LOCAL_SRC_FILES := \
-   img_process.c\
-   fb2png.c
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := fb2png
 
-LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
-   external/zlib \
-   external/libpng
+fb2png_src := \
+	fb2png.c \
+	img_process.c \
+	fb.c
 
-LOCAL_PRELINK_MODULE := false
+fb2png_includes := \
+	$(common_C_INCLUDES) \
+	external/zlib \
+	external/libpng
 
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_SRC_FILES := $(fb2png_src)
+LOCAL_C_INCLUDES += $(fb2png_includes)
+
+LOCAL_SHARED_LIBRARIES := libz libutils
+LOCAL_STATIC_LIBRARIES := libpng
+
+include $(BUILD_EXECUTABLE)
 
 # build jni
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += $(JNI_H_INCLUDE)
 
-LOCAL_MODULE := libascreenshooter
-LOCAL_SRC_FILES := fb2png-jni.c
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libfb2png
+LOCAL_SRC_FILES := \
+	fb2png-jni.c \
+	$(fb2png_src)
 
-LOCAL_STATIC_LIBRARIES := libfb2png libpng
+LOCAL_C_INCLUDES += $(fb2png_includes)
+LOCAL_STATIC_LIBRARIES := libpng
 LOCAL_SHARED_LIBRARIES := libz libutils
 
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := fb2png
-LOCAL_SRC_FILES := main.c
-
-LOCAL_SHARED_LIBRARIES := libz libutils
-LOCAL_STATIC_LIBRARIES := libfb2png libpng
-
-
-include $(BUILD_EXECUTABLE)
